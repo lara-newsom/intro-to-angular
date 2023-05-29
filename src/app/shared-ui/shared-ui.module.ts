92923c23dd8da15a2fc563c5d1e7a7bd3934ca9f
Lara <lara@laranewsom.com>
2023-05-28 22:34:02 -0500

    fixed a few things

--- src/app/shared-ui/shared-ui.module.ts
+++ src/app/shared-ui/shared-ui.module.ts
@@ -1,22 +1,21 @@
 import { NgModule } from '@angular/core';
-import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { RouterModule } from '@angular/router';
 import { SubHeroLogoModule } from './sub-hero-logo/sub-hero-logo.module';
+import { BreadcrumbsModule } from './breadcrumbs/breadcrumbs.module';
 
 @NgModule({
   declarations: [
-    BreadcrumbsComponent,
   ],
   exports: [
-    BreadcrumbsComponent,
   ],
   imports: [
     RouterModule,
     MatButtonModule,
     MatMenuModule,
     SubHeroLogoModule,
+    BreadcrumbsModule,
   ]
 })
 export class SharedUiModule {}
