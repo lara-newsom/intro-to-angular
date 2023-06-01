3ecace522dc3c257414c4e803afc1574ec92e6dd
Lara <lara@laranewsom.com>
2023-05-31 21:46:22 -0500

    deleted dead code

--- src/app/shared-ui/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared-ui/breadcrumbs/breadcrumbs.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { ProductService } from '../../services/product.service';
 import { ROUTE_TOKENS } from 'src/app/models/route-tokens';
 import { Breadcrumb } from 'src/app/models/breadcrumb';
 
@@ -10,11 +9,6 @@ import { Breadcrumb } from 'src/app/models/breadcrumb';
 })
 export class BreadcrumbsComponent{
   @Input({required: true}) breadcrumbs: Breadcrumb[] = [];
-  selectedCategory$ = this.productService.selectedCategory$;
-  selectedProduct$ = this.productService.selectedProduct$;
   readonly ROUTE_TOKENS = ROUTE_TOKENS;
 
-  constructor(
-    private readonly productService: ProductService,
-  ){}
 }
