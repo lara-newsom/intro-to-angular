77ade227dd2c3fa650eea1c6cbdf074a43c96acf
Lara <lara@laranewsom.com>
2023-06-02 22:42:48 -0500

    deleted event handler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  myEventHandler(name: string){
-    console.log('Event was emitted with value: ', name);
-  }
 }
