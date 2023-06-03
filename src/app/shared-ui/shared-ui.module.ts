834c2b5d599e130a9ec5501c64245d8077968555
Lara <lara@laranewsom.com>
2023-06-02 22:54:44 -0500

    added home child components

---
+++ src/app/shared-ui/shared-ui.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { SubSectionComponent } from './sub-section/sub-section.component';
+import { SubHeroLogoComponent } from './sub-hero-logo/sub-hero-logo.component';
+import { CardComponent } from './card/card.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeroSectionComponent,
+    SubSectionComponent,
+    SubHeroLogoComponent,
+    CardComponent,
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedUiModule { }
