5b154f8ca6320c5f00b34cb2078a9aa90a67696c
Lara <lara@laranewsom.com>
2023-06-03 18:22:09 -0500

    changes to services

--- src/app/services/product-api.service.ts
+++ src/app/services/product-api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Product } from '../models/product';
+import { map } from 'rxjs/operators';
 
 // this is the return type of the endpoint
 interface ProductsResponse {
@@ -12,10 +13,16 @@ interface ProductsResponse {
 })
 export class ProductApiService {
   // inject the HttpClient using the inject function
+  private readonly http = inject(HttpClient);
 
   // declare a constant for the URL endpoint
-  // private readonly endpointUrl = 'http://localhost:3000/products';
+  private readonly endpointUrl = 'http://localhost:3000/products';
 
   // create a public readonly property to hold the Observable returned by httpClient.get
   // we will use the map operator to unwrap the response in this service
+  readonly getProducts$ = this.http.get<ProductsResponse>(this.endpointUrl).pipe(
+    map((response) => response.products)
+  );
 }
+
+
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, map, of, switchMap } from 'rxjs';
+import { BehaviorSubject, map, tap } from 'rxjs';
 import { Product } from '../models/product';
-import { Category } from '../models/category';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { PRODUCTS } from '../models/product-data.mock';
 import { ProductApiService } from './product-api.service';
@@ -12,17 +11,32 @@ import { ProductApiService } from './product-api.service';
 export class ProductService {
   // create a private readonly property called products that is a BehaviorSubject
   // We will initialize the subject with the hardcoded PRODUCTS array
+  private readonly products = new BehaviorSubject<Product[]>(PRODUCTS);
 
   // expose a public property called products$ equal to the observable value of the products subject
+  readonly products$ = this.products.asObservable()
 
   // Let's change this property to return the first, middle, and last product
   // We will use the products response from productApiService.getProducts$
-  readonly homeProducts$ = of(PRODUCTS.slice(0, 3));
+  readonly homeProducts$ = this.products$.pipe(
+    map((products) => {
+      const middle = Math.floor(products.length / 2);
+
+      return [products[0], products[middle], products[products.length - 1]];
+    })
+  );
 
   // add a dependency for productApiService in the constructor
   // in the constructor method, subscribe to getProducts$
   // using the tap operator, call next on the products subject
   // use the takeUntilDestroyed operator to handle cleaning up the subscription
-  constructor() {
+  constructor(
+    private readonly productApiService: ProductApiService
+  ) {
+    this.productApiService.getProducts$.pipe(
+      tap((products) => this.products.next(products)),
+      takeUntilDestroyed()
+    ).subscribe();
   }
 }
+
