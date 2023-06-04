482329baeb704278c45f55b2714bce08fa37569b
Lara <lara@laranewsom.com>
2023-06-04 00:03:28 -0500

    added detail-view

---
+++ src/app/product-view/detail-view/detail-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-view',
+  templateUrl: './detail-view.component.html',
+  styleUrls: ['./detail-view.component.css']
+})
+export class DetailViewComponent {
+
+}
--- src/app/product-view/product-view.module.ts
+++ src/app/product-view/product-view.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DetailViewComponent } from './detail-view/detail-view.component';
 import { ProductViewComponent } from './product-view.component';
 
 
 
 @NgModule({
   declarations: [
-    ProductViewComponent
+    ProductViewComponent,
+    DetailViewComponent
   ],
   imports: [
     CommonModule
