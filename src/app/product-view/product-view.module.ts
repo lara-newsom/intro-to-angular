9498acab26e2cdca03386fce8cfb1411179b2502
Lara <lara@laranewsom.com>
2023-06-04 00:03:58 -0500

    added side menu

--- src/app/product-view/product-view.module.ts
+++ src/app/product-view/product-view.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DetailViewComponent } from './detail-view/detail-view.component';
 import { ProductViewComponent } from './product-view.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
 
 
 
 @NgModule({
   declarations: [
     ProductViewComponent,
-    DetailViewComponent
+    DetailViewComponent,
+    SideMenuComponent
   ],
   imports: [
     CommonModule
