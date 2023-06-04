26205504c1df3ce2fbad6f180258483e47c1334f
Lara <lara@laranewsom.com>
2023-06-04 00:02:13 -0500

    added product view

---
+++ src/app/product-view/product-view.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductViewComponent } from './product-view.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProductViewComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ProductViewModule { }
